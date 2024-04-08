8cf775f2e6a5d1ea003cdabf376db1a9585b8ec5
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2024-04-08 14:38:28 +0700

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from './assets/components/navbar/Navbar';
+
 function App() {
-  return <div className="App">abc</div>;
+  return (
+    <div className="App font-bold text-xl text-red-200">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/assets/components/navbar/Navbar.tsx
+++ src/assets/components/navbar/Navbar.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
 import './Navbar.css';
+// import logo from '@images/user/logo.png';
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        {/* <img src={logo} alt="Logo" /> */}
+        <p>SHOPPER</p>
+      </div>
+    </div>
+  );
 }
