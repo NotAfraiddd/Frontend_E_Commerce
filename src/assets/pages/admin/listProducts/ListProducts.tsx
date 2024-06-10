63d563d36d9cc615b98f45115a5b9bfb69e160cb
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2024-06-10 15:18:43 +0700

    edit scroll

--- src/assets/pages/admin/listProducts/ListProducts.tsx
+++ src/assets/pages/admin/listProducts/ListProducts.tsx
@@ -1,5 +1,5 @@
 import { cross_icon } from '@images/index'
-import { ChangeEvent, useEffect, useState } from 'react'
+import { ChangeEvent, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { RootState } from 'store'
@@ -47,51 +47,51 @@ export const ListProducts = () => {
       }
     }
     fetchProducts()
-  }, [])
+  }, [dispatch])
 
   return (
-    <div className='admin-list-products__contain overflow-auto w-full mt-8 mx-8 rounded-[20px] bg-white pb-8 px-10'>
-      <div className='sticky top-0 bg-white z-[1px] pt-8'>
-        <p className='mb-2'>Category type</p>
-        <select name='' id='' className='border rounded-md h-12 px-5 mb-5' onChange={getValueProductTpe}>
-          {listTypeProducts.map((product) => (
-            <option key={product.id} value={product.type}>
-              {product.type}
-            </option>
-          ))}
-        </select>
-        <div className='admin-list-products__title justify-items-center grid grid-cols-custom-6 overflow-x-auto text-base font-semibold gap-3 xl:gap-[75px] py-5 text-#[454545] items-center '>
-          <p>Product</p>
-          <p>Title</p>
-          <p>Category</p>
-          <p>New Price</p>
-          <p>Old Price</p>
-          <p>Remove</p>
-        </div>
-        <hr className='h-1 bg-[#e2e2e2] border-0' />
+    <div className='admin-list-products__contain w-full mt-8 mx-8 rounded-[20px] bg-white py-8 px-10'>
+      <p className='mb-2'>Category type</p>
+      <select name='' id='' className='border rounded-md h-12 px-5 mb-5' onChange={getValueProductTpe}>
+        {listTypeProducts.map((product) => (
+          <option key={product.id} value={product.type}>
+            {product.type}
+          </option>
+        ))}
+      </select>
+      <div className='admin-list-products__title justify-items-center grid grid-cols-custom-6 overflow-x-auto text-base font-semibold gap-3 xl:gap-[75px] py-5 text-#[454545] items-center '>
+        <p>Product</p>
+        <p>Title</p>
+        <p>Category</p>
+        <p>New Price</p>
+        <p>Old Price</p>
+        <p>Remove</p>
       </div>
-      {productList.map((ele, index) => (
-        <div
-          className='admin-list-products justify-items-center text-sm lg:text-base xl:text-lg grid grid-cols-custom-6 gap-3 xl:gap-[75px] py-5 text-#[454545] items-center'
-          key={index}
-        >
-          <img src={ele.image} alt='' className='h-16 w-16 object-contain' />
-          <p className='hover:underline cursor-pointer w-full' onClick={() => ele.id && clickToDetailProduct(ele.id)}>
-            {ele.name}
-          </p>
-          <p>{getTypeProduct(ele.category || 0)}</p>
-          <button className=' w-8 h-6 lg:w-16 lg:h-12 border-[#ebebeb] border-[2px]'>{ele.new_price}</button>
-          <button className=' w-8 h-6 lg:w-16 lg:h-12 border-[#ebebeb] border-[2px]'>{ele.old_price}</button>
-          <img
-            src={cross_icon}
-            onClick={() => {
-              ele.id && handleDeleteProduct(ele.id)
-            }}
-            alt='icon_remove'
-            className='cursor-pointer'
-          />
-        </div>
-      ))}
+      <hr className='h-1 bg-[#e2e2e2] border-0' />
+      <div className='overflow-auto product-list'>
+        {productList.map((ele, index) => (
+          <div
+            className='admin-list-products justify-items-center text-sm lg:text-base xl:text-lg grid grid-cols-custom-6 gap-3 xl:gap-[75px] py-5 text-#[454545] items-center'
+            key={index}
+          >
+            <img src={ele.image} alt='' className='h-16 w-16 object-contain' />
+            <p className='hover:underline cursor-pointer w-full' onClick={() => ele.id && clickToDetailProduct(ele.id)}>
+              {ele.name}
+            </p>
+            <p>{getTypeProduct(ele.category || 0)}</p>
+            <button className=' w-8 h-6 lg:w-16 lg:h-12 border-[#ebebeb] border-[2px]'>{ele.new_price}</button>
+            <button className=' w-8 h-6 lg:w-16 lg:h-12 border-[#ebebeb] border-[2px]'>{ele.old_price}</button>
+            <img
+              src={cross_icon}
+              onClick={() => {
+                ele.id && handleDeleteProduct(ele.id)
+              }}
+              alt='icon_remove'
+              className='cursor-pointer'
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
